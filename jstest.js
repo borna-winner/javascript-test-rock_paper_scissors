f0212c4ffd04c164ecccdb3b74adee8e71f89d2e
james <james@whygee.com>
2022-03-25 10:48:38 -0400

    fix syntac and added score system

--- jstest.js
+++ jstest.js
@@ -1,66 +1,80 @@
-let playroundresult = "";
+let playRoundResult = "";
 let score = 0;
-function playround(playerpick, computerplay) {
-    if(playerpick === ("rock") && computerplay() === ("rock")){
-        playroundresult = ("You draw! rock rock");
-        score =+ 0;
+function playRound(playerPick, computerPlay) {
+    if(playerPick === ("rock") && computerPlay() === ("rock")){
+        playRoundResult = ("You draw! rock rock");
+        score += 0;
     }
-    else if(playerpick === ("rock") && computerplay() === ("scissors")){
-        playroundresult = ("You win! rock beats scissors");
-        score =+ 1;
+    else if(playerPick === ("rock") && computerPlay() === ("scissors")){
+        playRoundResult = ("You win! rock beats scissors");
+        score += 1;
     }
-    else if(playerpick === ("rock") && computerplay() === ("paper")){
-        playroundresult = "You lose rock he got paper";
-        score =- 1;
+    else if(playerPick === ("rock") && computerPlay() === ("paper")){
+        playRoundResult = "You lose rock he got paper";
+        score -= 1;
     }
-    else if(playerpick === ("scissors") && computerplay() === ("paper")){
-        playroundresult = "You won! scissors he got paper";
-        score =+ 1;
+    else if(playerPick === ("scissors") && computerPlay() === ("paper")){
+        playRoundResult = "You won! scissors he got paper";
+        score += 1;
     }
-    else if(playerpick === ("scissors") && computerplay() === ("scissors")){
-        playroundresult = "You draw scissors scissors scissors";
-        score =+ 0;
+    else if(playerPick === ("scissors") && computerPlay() === ("scissors")){
+        playRoundResult = "You draw scissors scissors scissors";
+        score += 0;
     }
-    else if(playerpick === ("scissors") && computerplay() === ("rock")){
-        playroundresult = "You lose scicssors! he got rock";
-        score =- 1;
+    else if(playerPick === ("scissors") && computerPlay() === ("rock")){
+        playRoundResult = "You lose scicssors! he got rock";
+        score -= 1;
     }
-    else if(playerpick === ("paper") && computerplay() === ("rock")){
-        playroundresult = "You win!!! paper beats rock";
-        score =+ 1;
+    else if(playerPick === ("paper") && computerPlay() === ("rock")){
+        playRoundResult = "You win!!! paper beats rock";
+        score += 1;
     }
-    else if(playerpick === ("paper") && computerplay() === ("paper")){
-        playroundresult = "You draw! paper he got paper";
-        score =+ 0;
+    else if(playerPick === ("paper") && computerPlay() === ("paper")){
+        playRoundResult = "You draw! paper he got paper";
+        score += 0;
     }
-    else if(playerpick === ("paper") && computerplay() === ("scissors")){
-        playroundresult = "you lose paper he got scissors";
-        score =- 1;
+    else if(playerPick === ("paper") && computerPlay() === ("scissors")){
+        playRoundResult = "you lose paper he got scissors";
+        score -= 1;
     }
 }
-const myArray = ["rock", "paper", "scissors"];
-let playerpick = (prompt("please enter rock paper or scissors"));
+const choiceArray = ["rock", "paper", "scissors"];
+let playerPick = (prompt("please enter rock paper or scissors"));
 
-function computerplay() {
-  return myArray[~~(Math.random() * myArray.length)];
+function computerPlay() {
+  return choiceArray[~~(Math.random() * choiceArray.length)];
 }
 
-function playerplay() {
-    return playerpick = playerpick.toLowerCase();
+function playerPlay() {
+    return playerPick = playerPick.toLowerCase();
 }
-let playerinput = ("please enter rock paper or scissors");
+
+let playerInput = ("please enter rock paper or scissors");
+
+function scoreGame() {
+    if(score > 0) {
+        scoreResult = "ScoreWin";
+        return(scoreResult);
+    }
+    else if(score < 0) {
+        scoreResult = "scoreLose";
+        return(scoreResult);
+    }
+    else if(score === 0){
+        scoreResult = "OKK";
+    }
+    else{
+        return("What happened");
+    }
+}
 
 function game() {
     for (let i = 0; i < 4; i++) {
-        playerinput = (prompt("please enter rock paper or scissors"));
-        playerplay();
-        (playround(playerpick, computerplay)); 
-        console.log(playroundresult);
-        //if(score > 0){
-        //    console.log("You win!")
-        //}
-        //if(score < 0){
-        //    console.log("You lose.")
-        //}
+        playerInput = (prompt("please enter rock paper or scissors"));
+        playerPlay();   
+        (playRound(playerPick, computerPlay)); 
+        console.log(playRoundResult);
+        if(i === 4) {break;}
+        console.log(scoreGame());
 }
 }
